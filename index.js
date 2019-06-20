e908f1d13badaabfff9d49a6a710fde965d1c914
Jamie <vctqs1@gmail.com>
2019-06-20 11:52:42 +0700

    up database

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
 // const dbRoute = "mongodb://localhost:27017/extension-youtube";
-const dbRoute = "mongodb://@ds247670.mlab.com:47670/extension-youtube";
+const dbRoute = "mongodb://ds247670.mlab.com:47670/extension-youtube";
 const cors = require('cors')
 const Route = require('./router');
 mongoose.connect(
